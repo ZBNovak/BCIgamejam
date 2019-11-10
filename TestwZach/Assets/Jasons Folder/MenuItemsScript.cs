256f363ce3da832f6a0b1b1bc92a3eb72a473b53
Jason Raiti <jzraiti@gmail.com>
2019-11-10 13:40:55 -0500

    Start Menu with scripts for selecting "highlighted" sprites with spacebar button

---
+++ TestwZach/Assets/Jasons Folder/MenuItemsScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuItemsScript : MonoBehaviour
+{
+    public bool highlighted;
+
+    Animator animator;
+
+
+    void Start()
+    {
+        animator = GetComponent<Animator>(); 
+    }// this starts the "Idle" animation 
+
+    void Update()
+    {
+        if (highlighted==true)
+        {
+            animator.SetBool("Highlighted", true);
+        }
+        else
+        {
+            animator.SetBool("Highlighted", false);
+        }
+
+    }
+    // create a function that will , when highlighted = true , highlight the goddam fukin thing
+}
